dd7c78dad8f4c86f4f06c50c75e6ad26140eba99
Akshay <mone.akshay@gmail.com>
2026-02-26 16:16:02 +0000

    fixes from jules

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
 import 'react-native-url-polyfill/auto';
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { RootNavigator } from './src/shared/navigation/RootNavigator';
 
 export default function App() {
   return (
-    <>
+    <SafeAreaProvider>
       <StatusBar style="auto" />
       <RootNavigator />
-    </>
+    </SafeAreaProvider>
   );
 }
--- src/shared/components/SafeLayout.tsx
+++ src/shared/components/SafeLayout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, ViewStyle } from 'react-native';
+import { StyleSheet, ViewStyle } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { colors } from '../theme/colors';
 
 interface SafeLayoutProps {
