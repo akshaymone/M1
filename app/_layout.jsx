13abbc8cbf741417bb71230f15d05e98b9acc871
Akshay <mone.akshay@gmail.com>
2026-04-01 09:59:37 +0000

    fix: add babel, remove conflicting files, and improve layout

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,10 +1,14 @@
+import React from "react";
+import { View } from "react-native";
 import { Slot } from 'expo-router';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
   return (
+    <View style={{ flex: 1 }}>
     <SafeAreaProvider>
       <Slot />
+    </View>
     </SafeAreaProvider>
   );
 }
